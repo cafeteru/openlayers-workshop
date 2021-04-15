d86978a0b930608c76bf5c12c1ec811b78e833e7
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2021-04-15 12:58:57 +0200

    Change to scss

--- src/app/components/basic/basic.component.ts
+++ src/app/components/basic/basic.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import TileLayer from 'ol/layer/Tile';
+import Map from 'ol/Map';
+import * as olProj from 'ol/proj';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
 
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
-  styleUrls: ['./basic.component.css']
+  styleUrls: ['./basic.component.scss']
 })
 export class BasicComponent implements OnInit {
+  map: Map;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([7.0785, 51.4614]),
+        zoom: 5
+      })
+    });
   }
 
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 import { BasicComponent } from './basic/basic.component';
 import { ComponentsRoutingModule } from './components-routing.module';
 import { MenuComponent } from './menu/menu.component';
+import { Menu2Component } from './menu2/menu2.component';
 
 
 
 @NgModule({
   declarations: [
     BasicComponent,
-    MenuComponent
+    MenuComponent,
+    Menu2Component,
   ],
   imports: [
     ComponentsRoutingModule,
