22aab618b5a93e05174efbcfeefd83b1eee9d5df
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2021-04-19 13:30:50 +0200

    Add interactive

--- src/app/components/vector-tile-mapbox/vector-tile-mapbox-routing.module.ts
+++ src/app/components/vector-tile-mapbox/vector-tile-mapbox-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VectorTileComponent } from './vector-tile/vector-tile.component';
 import { MapboxComponent } from './mapbox/mapbox.component';
+import { InteractiveComponent } from './interactive/interactive.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
     {
         path: 'mapbox',
         component: MapboxComponent,
+    },
+    {
+        path: 'interactive',
+        component: InteractiveComponent,
     },
 ];
 
--- src/app/components/vector-tile-mapbox/vector-tile-mapbox.module.ts
+++ src/app/components/vector-tile-mapbox/vector-tile-mapbox.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { VectorTileComponent } from './vector-tile/vector-tile.component';
 import { VectorTileMapboxRoutingModule } from './vector-tile-mapbox-routing.module';
 import { MapboxComponent } from './mapbox/mapbox.component';
+import { InteractiveComponent } from './interactive/interactive.component';
 
 
 @NgModule({
   declarations: [
     VectorTileComponent,
-    MapboxComponent
+    MapboxComponent,
+    InteractiveComponent
   ],
   imports: [
     VectorTileMapboxRoutingModule,
