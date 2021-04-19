d788722a82a6934a25e6796759715b84809da59e
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2021-04-19 16:07:48 +0200

    Add map setup

--- src/app/components/raster/map-setup/map-setup.component.ts
+++ src/app/components/raster/map-setup/map-setup.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZSource from 'ol/source/XYZ';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-map-setup',
@@ -6,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-setup.component.scss']
 })
 export class MapSetupComponent implements OnInit {
+  private map: Map;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.map = new Map({
+      target: 'map-container-raster',
+      layers: [
+        new TileLayer({
+          source: new XYZSource({
+            url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
+          })
+        })
+      ],
+      view: new View({
+        center: fromLonLat([-117.1493, 32.6965]),
+        zoom: 12
+      })
+    });
   }
 
 }
